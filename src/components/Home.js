b51fc21e963fb13aeed6fc1e04ac36656ae7f94d
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-02 16:37:24 -0600

    Room Component and Context Room

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import Hero from './Hero'
 import Banner from './Banner'
 import { Link } from 'react-router-dom'
 import Services from './Services'
+import FeaturedRooms from './FeaturedRooms'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         </Banner>
       </Hero>
       <Services/>
+      <FeaturedRooms/>
     </>
   )
 }
