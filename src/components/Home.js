5a50f958d1dc22617dbf1c020de861d44d1fdb1d
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-02 14:50:09 -0600

    Services Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,19 @@ import React from 'react'
 import Hero from './Hero'
 import Banner from './Banner'
 import { Link } from 'react-router-dom'
+import Services from './Services'
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner title='luxurious rooms' subtitle='delux rooms starting at $299'>
-        <Link to='/rooms' className='btn-primary'>
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title='luxurious rooms' subtitle='delux rooms starting at $299'>
+          <Link to='/rooms' className='btn-primary'>
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services/>
+    </>
   )
 }
