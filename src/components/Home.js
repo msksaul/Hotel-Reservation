4f880fa54be44f00053229371774c0080a90bd11
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-02 14:32:44 -0600

    Banner Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from './Hero'
+import Banner from './Banner'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <Hero/>
+    <Hero>
+      <Banner title='luxurious rooms' subtitle='delux rooms starting at $299'>
+        <Link to='/rooms' className='btn-primary'>
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
