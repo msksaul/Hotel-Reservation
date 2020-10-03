220245f4c90adcb563411f5a4756393fce6ea8d1
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-03 16:35:10 -0600

    Environment Variables

--- src/contentful/index.js
+++ src/contentful/index.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful'
 
 export default createClient({
-  space: 'zyb09nxjvbxz',
-  accessToken: '6_zgfUkGSYX1DWzqMM7wJLrB68_by2x3r19YFAmWWzM'
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
